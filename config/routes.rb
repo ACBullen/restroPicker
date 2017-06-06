94fe907f8057b697c53fc8f680cdde0bec5d5d26
Alexander Bullen <acbullen1990@gmail.com>
2017-06-05 17:49:32 -0700

    Remove now extraneous filter controller. Set commented out route for rankings for future use

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create, :update]
     resources :groups, only: [:create, :update, :show]
-    resources :filters, only: [:create, :destroy]
+    # resources :rankings, only: [:create, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount ActionCable.server => '/cable'
