ce5a7d442b24f0d0e96a50b0bdbc8ddaf8c0122b
Alexander Bullen <acbullen1990@gmail.com>
2017-06-06 13:12:18 -0700

    Create and block out create method on rankings controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     resources :users, only: [:create, :update]
     resources :groups, only: [:create, :update, :show]
     resources :restaurants, only: [:create]
-    # resources :rankings, only: [:create, :destroy]
+    resources :rankings, only: [:create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount ActionCable.server => '/cable'
