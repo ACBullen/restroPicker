56272083e28b6cb59d6f271eb76db21aeb856319
Alissara <alissara.r@gmail.com>
2017-06-07 16:57:04 -0700

    Add create group api util

--- frontend/restroPicker/src/App.js
+++ frontend/restroPicker/src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import configureStore from './store/store';
 import Splash from './components/Splash';
-
 
 class App extends Component {
 
-  render () {
+  render() {
+    // const store = configureStore();
     return (
-      <View style={{ flex: 1 }}>
+      // <Provider store={store}>
         <Splash />
-      </View>
+      // </Provider>
     );
   }
 }
