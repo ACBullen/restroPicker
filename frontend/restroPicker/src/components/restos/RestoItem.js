50f95c49c572ae32c7b46f3b524e995cb5c96875
Alex E Scott <aescott@outlook.com>
2017-06-10 16:33:13 -0700

    add listitem boilerplate

---
+++ frontend/restroPicker/src/components/restos/RestoItem.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class RestoItem extends Component {
+  render() {
+
+  }
+}
+
+export default RestoItem
--- frontend/restroPicker/src/components/restos/RestoList.js
+++ frontend/restroPicker/src/components/restos/RestoList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { ListView } from 'react-native';
 import axios from 'axios';
 
 class RestoList extends Component {
@@ -9,23 +10,27 @@ class RestoList extends Component {
 
   componentWillMount() {
     let group_id = this.props.group.id;
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
     setTimeout(() =>    axios.get(`http://localhost:3000/api/restaurants?group=${group_id}`)
-      .then(response => console.log(response)), 1000)
-    ;
-}
+      .then(response => {
+        this.dataSource = ds.cloneWithRows(response.data.restaurants);
+      })
+    );
+  }
 
-
+  renderRow() {
+    
+  }
 
   render() {
     return (
-      <View>
-        <Text>RestoList!!</Text>
-        <Text>RestoList!!</Text>
-        <Text>RestoList!!</Text>
-        <Text>RestoList!!</Text>
-        <Text>RestoList!!</Text>
-        <Text>RestoList!!</Text>
-      </View>
+      <ListView
+        dataSource={this.dataSource}
+        renderRow ={this.renderRow}
+
+      />
     );
   }
 }
