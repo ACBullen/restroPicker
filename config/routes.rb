dd86d09357daadbb9cfeccfd26b6bfcc0e0b92ff
Alex E Scott <aescott@outlook.com>
2017-06-10 17:36:10 -0700

    pretest commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create, :update]
     resources :groups, only: [:create, :update, :show]
-    resources :restaurants, only: [:create]
+    resources :restaurants, only: [:create, :index]
     resources :rankings, only: [:create]
     resources :results, only: [:show]
   end
