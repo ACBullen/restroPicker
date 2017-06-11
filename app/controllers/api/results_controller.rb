f2a71d087eb4dfdce6b44690f62bb78ec75713da
Alexander Bullen <acbullen1990@gmail.com>
2017-06-10 21:28:23 -0700

    Operationalize sorting algo in ruby

--- app/controllers/api/results_controller.rb
+++ app/controllers/api/results_controller.rb
@@ -9,5 +9,40 @@ class Api::ResultsController < ApplicationController
 
   def best_choice_algo(restaurants)
     #Alex Scott's sorting algorithm gets executed here.
+    rest_options = {}
+    restaurants.each do |rest|
+      rest_options[rest.id] = []
+      rest.rankings.each do |ranking|
+        rest_options[rest.id] << ranking.rank
+      end
+    end
+    values = getPowerValues(rest_options)
+    rankTotals = {}
+
+    rest_options.each do |key, val|
+      n = val.length
+      total = 0
+      while n > 0
+        rank_to_be_added = val[n - 1]
+        value_to_be_added = values[rank_to_be_added - 1]
+        total += value_to_be_added
+        n -= 1
+      end
+      rankTotals[key] = total
+    end
+    return rankTotals.to_a.sort {|a,b| b[1] <=> a[1]}.map{|a| a[0]}
+  end
+
+  def getPowerValues(options)
+    power-values = []
+    n = options.keys.length
+    value = 1
+    while n > 0
+      powerValues.push(value)
+      n -= 1
+      value /= 2
+    end
+
+    return powerValues
   end
 end
