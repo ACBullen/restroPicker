454bcab9ee45f0a018592360f478c5a9eac09df8
Alexander Bullen <acbullen1990@gmail.com>
2017-07-05 11:20:38 -0700

    Pausing to shift branch

--- frontend/restroPicker/src/components/waitroom/WaitRoom.js
+++ frontend/restroPicker/src/components/waitroom/WaitRoom.js
@@ -20,7 +20,7 @@ class WaitRoom extends Component {
     // this.endcheck = global.setInterval(this.fetchData.bind(this), 1000);
     this.ws = new WebSocket("ws://resto-pick.herokuapp.com/cable")
     this.ws.onerror = (m) => console.log(m);
-    this.ws.onmessage = (m) => JSON.parse(m.data).type !== "ping" ? console.log(m.data) : "";
+    this.ws.onmessage = (m) => JSON.parse(m.data).type !== "ping" ? console.log(m.data) : console.log("ping");
     this.ws.onopen = () => {this.ws.send(JSON.stringify({"command":"subscribe","identifier":"{\"channel\":\"GroupChannel\"}"}))};
   }
 
